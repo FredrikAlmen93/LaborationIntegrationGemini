d717e8c1c0aeb9f557b020dd1765cf259fff7b42
FredrikAlmen93 <fredrik.x.almen@gmail.com>
2026-04-28 10:59:49 +0200

    initialize LaborationIntegrationGemini project with Maven and Spring Boot setup

---
+++ src/main/java/se/iths/fredrik/laborationintegrationgemini/LaborationIntegrationGeminiApplication.java
@@ -0,0 +1,13 @@
+package se.iths.fredrik.laborationintegrationgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaborationIntegrationGeminiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaborationIntegrationGeminiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/fredrik/laborationintegrationgemini/LaborationIntegrationGeminiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.fredrik.laborationintegrationgemini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaborationIntegrationGeminiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
